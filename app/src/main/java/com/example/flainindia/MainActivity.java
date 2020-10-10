4ad6fd37bec564cd47b1848067b5f83daf32943b
karsh <soni.karsh53@gmail.com>
2020-10-10 14:14:54 +0530

    Changed splash screen and in main screen added progressbar.

--- app/src/main/java/com/example/flainindia/MainActivity.java
+++ app/src/main/java/com/example/flainindia/MainActivity.java
@@ -15,6 +15,7 @@ import com.google.android.gms.ads.AdRequest;
 import com.google.android.gms.ads.AdView;
 import com.google.android.gms.ads.InterstitialAd;
 import com.google.android.gms.ads.MobileAds;
+import com.victor.loading.rotate.RotateLoading;
 
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
@@ -23,6 +24,7 @@ import java.util.concurrent.TimeUnit;
 public class MainActivity extends AppCompatActivity {
     private WebView webview;
     private AdView mAdView;
+    private RotateLoading circleProgressing;
 
     //ad
     private InterstitialAd mInterstitialAd;
@@ -35,11 +37,16 @@ public class MainActivity extends AppCompatActivity {
         //banner ad
         MobileAds.initialize(this, "ca-app-pub-6347747486396065~2989253167");
         mAdView = findViewById(R.id.adView);
+        circleProgressing = findViewById(R.id.circleProgress);
+        if(!circleProgressing.isStart()){
+            circleProgressing.start();
+        }
         AdRequest adRequest = new AdRequest.Builder().build();
         mAdView.loadAd(adRequest);
         //ad finish
 
         webview=(WebView) findViewById(R.id.webview);
+
         webview.setWebViewClient(new mywebClient());
         webview.loadUrl("https://www.theflain.com/");
         WebSettings webSettings=webview.getSettings();
@@ -88,6 +95,12 @@ public class MainActivity extends AppCompatActivity {
         //public void onPageStarted(WebView view, String url, Bitmap favicon){
             //super.onPageStarted(view,url,favicon);
         //}
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            circleProgressing.stop();
+        }
 
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
--- app/src/main/java/com/example/flainindia/SplashScreen.java
+++ app/src/main/java/com/example/flainindia/SplashScreen.java
@@ -4,15 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class SplashScreen extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
-        startActivity(intent);
-        finish();
+        setContentView(R.layout.screen_splash);
+
+        new Handler().postDelayed(
+                new Runnable() {
+                    @Override
+                    public void run() {
+                        Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                        startActivity(intent);
+                        SplashScreen.this.finish();
+                    }
+                }
+        , 2000);
     }
 }
 //android:width="300dp"
